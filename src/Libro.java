877d4d6a3f7f4e1e8a7b2e13f3d1a9810cd0e3ff
Cjcm <ccastillom1@gmail.com>
2026-05-07 17:43:17 -0500

    Añade el constructor, atributos básicos e implementa getters y setters en Libro

--- src/Libro.java
+++ src/Libro.java
@@ -1,2 +1,36 @@
+import java.util.Date;
+
 public class Libro {
+
+    //Atributos
+    private String title;
+    private String author;
+    public Date realeaseDate;
+
+    //Constructor
+    public Libro(String title, String author, Date realeaseDate){
+        this.setTitle(title);
+        this.setAuthor(author);
+        this.realeaseDate = realeaseDate;
+    }
+
+    //Métodos
+
+    //Getters
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return  this.author;
+    }
+
+    //Setters
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
 }
