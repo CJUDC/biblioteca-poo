d96e3702ed1f39293ddbdcf694f387456e0048f3
Cjcm <ccastillom1@gmail.com>
2026-05-07 17:56:06 -0500

    Implementación de getters y setters para Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -3,4 +3,35 @@ public class Usuario {
     private String name;
     private String userName;
     private String password;
+
+    //Métodos
+
+    //Getters
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserName(){
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    //Setters
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
