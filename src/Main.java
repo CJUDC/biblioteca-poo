96d6e5170714433b424575209c382b134e40f8e7
Cjcm <ccastillom1@gmail.com>
2026-05-16 16:32:26 -0500

    traspaso del menú en Main a la clase Menu

--- src/Main.java
+++ src/Main.java
@@ -1,96 +1,13 @@
+import view.Menu;
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Main menú
-        var scanner = new Scanner(System.in);
-        int opc;
+        Menu menu = new Menu();
+        menu.iniciar();
 
-        var bibi = new Biblioteca();
-
-
-        do{
-
-            System.out.println("\n--- Menú de opciones ---");
-            System.out.println("1. Registrar Libro");
-            System.out.println("2. Registrar Usuario");
-            System.out.println("3. Prestar Libro");
-            System.out.println("4. Devolver Libro");
-            System.out.println("5. Ver Libro");
-            System.out.println("6. Ver Usuario");
-            System.out.println("7. Ver Prestamo");
-            System.out.println("0. Salir");
-
-            System.out.println("Elija una opción:");
-            opc = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (opc){
-                case 1:
-                    System.out.println("Registra Libros");
-                    bibi.addBook();
-                    pausar(scanner);
-                    break;
-                case 2:
-                    System.out.println("Registra usuario");
-                    bibi.addUser();
-                    pausar(scanner);
-                    break;
-                case 3:
-                    System.out.println("logica prestamo");
-                    pausar(scanner);
-                    break;
-                case 4:
-                    System.out.println("logica Devolución");
-                    pausar(scanner);
-                    break;
-                case 5:
-                    System.out.println("Logica Ver libro");
-                    bibi.displayBook();
-                    pausar(scanner);
-                    break;
-                case 6:
-                    System.out.println("Logica Ver Usuario");
-                    bibi.displayUser();
-                    pausar(scanner);
-                    break;
-                case 7:
-                    System.out.println("Logica Ver prestamo");
-                    pausar(scanner);
-                    break;
-                case 0:
-                    System.out.println("Has salido");
-                    break;
-                default:
-                    System.out.println("\nOpción inválida");
-                    pausar(scanner);
-            }
-
-        } while(opc != 0);
-        scanner.close();
-
-    }
-
-    // Método para pausar y esperar al usuario
-    public static void pausar(Scanner sc) {
-        System.out.println("\nPresione Enter para volver al menú...");
-        sc.nextLine();
-    }
-
-    // Método para limpiar según el sistema operativo
-    public static void limpiarPantalla() {
-        try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-            }
-        } catch (Exception e) {
-            // Si falla, simplemente imprime líneas en blanco para "desplazar" el texto
-            for (int i = 0; i < 50; i++) System.out.println();
-        }
     }
 }
--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,5 +1,73 @@
 package view;
 
+import java.util.Scanner;
+
 public class Menu {
-    
+
+    //Main menú
+    Scanner scanner = new Scanner(System.in);
+    int opc;
+
+
+    public void iniciar() {
+        do {
+            System.out.println("===== BIBLIOTECA =====");
+            System.out.println("1. Gestión de Libros");
+            System.out.println("2. Gestión de Usuarios");
+            System.out.println("3. Gestión de Prestamos");
+            System.out.println("0. Salir");
+
+            System.out.println("Elija una opción:");
+            opc = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opc) {
+                case 1:
+                    System.out.println("Registra Libros");
+                    pausar(scanner);
+                    break;
+                case 2:
+                    System.out.println("Registra usuario");
+                    pausar(scanner);
+                    break;
+                case 3:
+                    System.out.println("logica prestamo");
+                    pausar(scanner);
+                    break;
+                case 4:
+                    System.out.println("logica Devolución");
+                    pausar(scanner);
+                    break;
+                case 0:
+                    System.out.println("Has salido!");
+                    break;
+                default:
+                    System.out.println("\nOpción inválida!!!");
+
+            }
+
+        } while (opc != 0);
+        scanner.close();
+    }
+
+    // Método para pausar y esperar al usuario
+    public static void pausar(Scanner sc) {
+        System.out.println("\nPresione Enter para volver al menú...");
+        sc.nextLine();
+    }
+
+    // Método para limpiar según el sistema operativo
+    public static void limpiarPantalla () {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            // Si falla, simplemente imprime líneas en blanco para "desplazar" el texto
+            for (int i = 0; i < 50; i++) System.out.println();
+        }
+    }
 }
