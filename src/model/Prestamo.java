d38c7fdbd3b611f21139d6e08e92c0afacbf5f5a
Cjcm <ccastillom1@gmail.com>
2026-05-20 13:13:55 -0500

    Se añade el atributo fecha en Prestamo

--- src/model/Prestamo.java
+++ src/model/Prestamo.java
@@ -1,17 +1,21 @@
 package model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Prestamo {
   private int prestamoId;
   public Usuario usuario;
   public Libro libro;
   public boolean state;
+  public Date fecha;
 
-  public Prestamo(Usuario usuario, Libro libro, boolean state){
+  public Prestamo(Usuario usuario, Libro libro, boolean state, Date fecha){
     this.usuario = usuario;
     this.libro = libro;
     this.state = state;
+    this.fecha = fecha;
   }
 
   //Getters
